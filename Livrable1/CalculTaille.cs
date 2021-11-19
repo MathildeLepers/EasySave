2d7159697b345ba6c11fffdff73a588dd615a3c0
MathildeLepers <mathilde.lepers@viacesi.fr>
2021-11-19 12:33:44 +0100

    Comment Calcultaille

--- Livrable1/CalculTaille.cs
+++ Livrable1/CalculTaille.cs
@@ -3,29 +3,29 @@ using System.IO;
 
 public class CalculTaille
 {
-    public float calculateFolderSize(string folder)
+    public float calculateFolderSize(string folder) //calculates the size of the directory to be backed up and starts the backup
     {
-        float folderSize = 0.0f;
+        float folderSize = 0.0f; //initialize directory size to 0
         try
         {
             //Checks if the path is valid or not
-            if (!Directory.Exists(folder))
-                return folderSize;
+            if (!Directory.Exists(folder))//if the directory doesn't exists 
+                return folderSize; //return size O
             else
             {
                 try
                 {
-                    foreach (string file in Directory.GetFiles(folder))
+                    foreach (string file in Directory.GetFiles(folder)) //for each file in the repository
                     {
-                        if (File.Exists(file))
+                        if (File.Exists(file)) //if the file exists 
                         {
                             FileInfo finfo = new FileInfo(file);
-                            folderSize += finfo.Length;
+                            folderSize += finfo.Length; //add to the total size the size of the file
                         }
                     }
 
                     foreach (string dir in Directory.GetDirectories(folder))
-                        folderSize += calculateFolderSize(dir);
+                        folderSize += calculateFolderSize(dir); //search if the repository contains an other repository with other file to count
                 }
                 catch (NotSupportedException e)
                 {
@@ -35,7 +35,7 @@ public class CalculTaille
         }
         catch (UnauthorizedAccessException e)
         {
-            Console.WriteLine("Unable to calculate folder size: {0}", e.Message);
+            Console.WriteLine("Unable to calculate folder size: {0}", e.Message); //catch if we don't have the autorization
         }
         return folderSize;
     }
