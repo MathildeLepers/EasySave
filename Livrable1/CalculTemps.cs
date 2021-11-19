85414e0cb3251b9a9d793ee915e8e5bcb25a0188
MathildeLepers <mathilde.lepers@viacesi.fr>
2021-11-19 14:26:53 +0100

    commentaire du calcul temps et lancement sauvegarde

--- Livrable1/CalculTemps.cs
+++ Livrable1/CalculTemps.cs
@@ -7,19 +7,19 @@ public class CalculTemps
     public double temps;
     public ModeleSauvegarde modeleSauv;
 
-    public CalculTemps(string source, string dest, Type typeSauv)
+    public CalculTemps(string source, string dest, Type typeSauv) //calculate the time and start the backup
     {
-        Stopwatch sw = Stopwatch.StartNew();
-        this.modeleSauv = new ModeleSauvegarde();
+        Stopwatch sw = Stopwatch.StartNew(); //start a counter 
+        this.modeleSauv = new ModeleSauvegarde(); //create a backup 
         if (typeSauv == Type.COMPLET)
         {
-            this.modeleSauv.SauvegardeComplete(source, dest, true);
+            this.modeleSauv.SauvegardeComplete(source, dest, true);//lauch complete backup
         }
         if (typeSauv == Type.DIFFERENTIEL)
         {
-            this.modeleSauv.SauvegardeDiff(source, dest, true);
+            this.modeleSauv.SauvegardeDiff(source, dest, true);//launch diff backup
         }
         sw.Stop();
-        this.temps = sw.Elapsed.TotalMilliseconds;
+        this.temps = sw.Elapsed.TotalMilliseconds;//stop counter and return time use to do the backup
     }
 }
