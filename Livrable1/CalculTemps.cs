6209eb0dad3f4cadaf5be9e3c1819a137b6d7751
MathildeLepers <mathilde.lepers@viacesi.fr>
2021-11-19 14:22:42 +0100

    mise à jour calcul temps

--- Livrable1/CalculTemps.cs
+++ Livrable1/CalculTemps.cs
@@ -9,21 +9,17 @@ public class CalculTemps
 
     public CalculTemps(string source, string dest, Type typeSauv)
     {
+        Stopwatch sw = Stopwatch.StartNew();
+        this.modeleSauv = new ModeleSauvegarde();
         if (typeSauv == Type.COMPLET)
         {
-            Stopwatch sw = Stopwatch.StartNew();
-            this.modeleSauv = new ModeleSauvegarde();
             this.modeleSauv.SauvegardeComplete(source, dest, true);
-            sw.Stop();
-            this.temps = sw.Elapsed.TotalMilliseconds;
         }
         if (typeSauv == Type.DIFFERENTIEL)
         {
-            Stopwatch sw = Stopwatch.StartNew();
-            this.modeleSauv = new ModeleSauvegarde();
             this.modeleSauv.SauvegardeDiff(source, dest, true);
-            sw.Stop();
-            this.temps = sw.Elapsed.TotalMilliseconds;
         }
+        sw.Stop();
+        this.temps = sw.Elapsed.TotalMilliseconds;
     }
 }
