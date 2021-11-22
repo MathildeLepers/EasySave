ab5f831cea0a1e25a623d42d0b95e72b12e09072
MathildeLepers <mathilde.lepers@viacesi.fr>
2021-11-22 12:34:14 +0100

    mise à jour

--- Livrable1/CalculTemps.cs
+++ Livrable1/CalculTemps.cs
@@ -8,20 +8,13 @@ public class CalculTemps
     public ModeleSauvegarde modeleSauv;
     public Sauvegarde sauvegarde;
 
-    public CalculTemps(Sauvegarde sauvegarde, Type typeSauv) //calculate the time and start the backup
+    public CalculTemps(Sauvegarde sauvegarde) //calculate the time and start the backup
     {
         this.sauvegarde = sauvegarde;
         this.sauvegarde.etat = Etat.ENCOURS;
         Stopwatch sw = Stopwatch.StartNew(); //start a counter 
         this.modeleSauv = new ModeleSauvegarde(); //create a backup 
-        if (typeSauv == Type.COMPLET)
-        {
-            this.modeleSauv.SauvegardeComplete(this.sauvegarde.source, this.sauvegarde.dest, true);//lauch complete backup
-        }
-        if (typeSauv == Type.DIFFERENTIEL)
-        {
-            this.modeleSauv.SauvegardeDiff(this.sauvegarde.source, this.sauvegarde.dest, true);//launch diff backup
-        }
+        this.modeleSauv.SauvegardeComplete(this.sauvegarde.source, this.sauvegarde.dest, true);//lauch complete backup
         sw.Stop();
         this.sauvegarde.etat = Etat.END;
         this.temps = sw.Elapsed.TotalMilliseconds;//stop counter and return time use to do the backup
