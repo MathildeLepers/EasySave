22b2243964728fa55551259673ad4911490e3d4e
just7juba <56232564+just7juba@users.noreply.github.com>
2021-12-06 23:29:35 +0100

    Add files via upload

---
+++ EasySave-wpf_fini3/Modele/sauvegarde.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Livrable2.Modele;
+using System.Diagnostics;
+
+namespace Livrable2.Modele
+{
+    public class sauvegarde
+    {
+        private string source;
+        public static State etat_file;
+        public static int nbfile;
+        private string destination;
+        private string nom;
+        private string type;
+        private static double tmp;
+
+
+        public static void sauvegarde_complet(string source, string dest)
+        {
+            etat_file = State.INPROGRESS;
+
+            foreach (var directory in Directory.GetDirectories(source))
+            {
+                string dirName = Path.GetFileName(directory);
+                if (!Directory.Exists(Path.Combine(dest, dirName)))
+                {
+                    Directory.CreateDirectory(Path.Combine(dest, dirName));
+                    Console.Write(Path.Combine(dest, dirName));
+                }
+                sauvegarde_complet(directory, Path.Combine(dest, dirName));
+            }
+            nbfile = 0;
+            foreach (var file in Directory.GetFiles(source))
+            {
+                File.Copy(file, Path.Combine(dest, Path.GetFileName(file)));
+                nbfile++;
+            }
+            etat_file = State.END;
+
+
+        }
+        public static void sauvegarde_complet_Crypt(string source, string dest)
+        {
+            etat_file = State.INPROGRESS;
+
+            foreach (var directory in Directory.GetDirectories(source))
+            {
+                string dirName = Path.GetFileName(directory);
+                if (!Directory.Exists(Path.Combine(dest, dirName)))
+                { 
+                    Directory.CreateDirectory(Path.Combine(dest, dirName));
+                    Console.Write(Path.Combine(dest, dirName));
+                }
+                sauvegarde_complet(directory, Path.Combine(dest, dirName));
+            }
+            nbfile = 0;
+            foreach (var file in Directory.GetFiles(source))
+            {
+
+                Process process = new Process();
+                string arg = source + Path.GetFileName(file) + " " + dest + Path.GetFileName(file);
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+                process.StartInfo.FileName = @"C:\Users\juba dahoumane\Desktop\EasySave-wpf_fini3\Cryptosoft\bin\Debug\netcoreapp3.1\Cryptosoft.exe";
+                process.StartInfo.Arguments = arg;
+              
+                process.Start();
+                process.WaitForExit();
+
+               
+                nbfile++;
+            }
+            etat_file = State.END;
+
+
+        }
+
+        public static void sauvegarde_differentiel()
+        {
+
+        }
+
+
+        public void set_source(string source)
+        {
+            this.source = source;
+        }
+
+        public void set_destination(string destination)
+        {
+            this.destination = destination;
+        }
+
+        public void set_nom(string nom)
+        {
+            this.nom = nom;
+        }
+
+        public void set_type(string type)
+        {
+            this.type = type;
+        }
+
+        public void set_tmp(double temp)
+        {
+            tmp = temp;
+        }
+
+        public string get_source()
+        {
+            return this.source;
+        }
+
+        public string get_destination()
+        {
+            return this.destination;
+        }
+        public string get_nom()
+        {
+            return this.nom;
+        }
+
+        public string get_type()
+        {
+            return this.type;
+        }
+    }
+}
